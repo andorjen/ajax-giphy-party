dfa3742e8f5039bccc7053b5e48c385ea74880be
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-08-31 16:15:49 -0700

    adding functions to get image URL from giphy

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ $("body").css("backgroundColor", "black")
     .css("color", "white")
     .css("textAlign", "center")
 
+function getFormValues () {
+    let $submittedVal = $("#gif-input").val();
+    return $submittedVal;
+}
+
+async function getGiphy () {
+    let value = getFormValues();
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q: value, api_key:MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym}});
+    return response.data.data[0].url;
+}
+
+$(".gif-form").on("submit", )
+
+
+
+
