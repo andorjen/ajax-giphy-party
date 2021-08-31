f8ae710e8b6e488d525f05a01cb65f5062171f21
Jenny Liu <jennyliuye@gmail.com>
2021-08-31 16:49:45 -0700

    basic functionality done

--- app.js
+++ app.js
@@ -4,18 +4,42 @@ $("body").css("backgroundColor", "black")
     .css("color", "white")
     .css("textAlign", "center")
 
-function getFormValues () {
+function getFormValues() {
     let $submittedVal = $("#gif-input").val();
     return $submittedVal;
 }
 
-async function getGiphy () {
+async function getGiphy() {
     let value = getFormValues();
-    let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q: value, api_key:MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym}});
-    return response.data.data[0].url;
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: value, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" } });
+    // console.log(response.data.data[0].images.original.url)
+    return response.data.data[0].images.original.url;
 }
 
-$(".gif-form").on("submit", )
+async function addGiphy(evt) {
+    evt.preventDefault();
+    let gifUrl = await getGiphy();
+
+    // console.log(gifUrl)
+
+    let gif = $("<img>")
+    gif.addClass("gif").attr("src", gifUrl)
+
+    console.log(gif)
+    let displayContainer = $("#gif-display")
+    displayContainer.append(gif)
+}
+
+$(".gif-form").on("submit", addGiphy)
+
+$("#gif-clear").on("click", clearDisplay)
+
+function clearDisplay() {
+    $("#gif-display").html("")
+}
+
+
+
 
 
 
