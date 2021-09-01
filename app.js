61b172f5719b37bb174992b00e95360c2b2ec773
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-08-31 17:21:39 -0700

    adding docstrings and global variables

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
+"use strict"
+
 console.log("Let's get this party started!");
+
+const GIPHY_URL = "http://api.giphy.com/v1/gifs/search"; 
+const GIPHY_API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
 
 $("body").css("backgroundColor", "black")
     .css("color", "white")
     .css("textAlign", "center")
 
+/** Get and return values from the form submit*/
 function getFormValues() {
     let $submittedVal = $("#gif-input").val();
     return $submittedVal;
 }
 
+/** Call Giphy's API using the form value and return a random URL  */
 async function getGiphy() {
     let value = getFormValues();
-    let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: value, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" } });
+    let response = await axios.get(GIPHY_URL, { params: { q: value, api_key: GIPHY_API_KEY } });
 
     let randomNum = Math.floor(Math.random() * response.data.data.length);
     return response.data.data[randomNum].images.original.url;
 }
 
+/** Add the gif to the HTML */
 async function addGiphy(evt) {
     evt.preventDefault();
     let gifUrl = await getGiphy();
@@ -38,8 +46,9 @@ $(".gif-form").on("submit", addGiphy)
 
 $("#gif-clear").on("click", clearDisplay)
 
+/** Clear the gifs displayed */
 function clearDisplay() {
-    $("#gif-display").html("")
+    $("#gif-display").empty();
 }
 
 
